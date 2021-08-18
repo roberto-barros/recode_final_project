40a0832b85ba290c22d8ab7937b4bc6912aabee4
roberto-barros <jroberto.barros2506@gmail.com>
2021-08-18 12:29:36 -0300

    Implements layoutActivity and create activities

---
+++ app/src/main/java/com/example/recode_final_project/views/DepartmentActivity.java
@@ -0,0 +1,16 @@
+package com.example.recode_final_project.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.recode_final_project.R;
+
+public class DepartmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_department);
+    }
+}
