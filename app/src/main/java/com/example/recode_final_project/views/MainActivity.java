a0553cc1695dc6dc78593792f382f14696d1ba0b
roberto-barros <jroberto.barros2506@gmail.com>
2021-08-18 17:38:01 -0300

    Implements activity_main.xml and onClick methods

--- app/src/main/java/com/example/recode_final_project/views/MainActivity.java
+++ app/src/main/java/com/example/recode_final_project/views/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.recode_final_project.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.recode_final_project.R;
 
@@ -12,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openDepartment(View view) {
+
+        Intent messenger = new Intent(this, DepartmentActivity.class);
+        messenger.putExtra("switchView", "");
+
+        startActivity(messenger);
+    }
+
+    public void openProfessor(View view) {
+
+        Intent messenger = new Intent(this, ProfessorActivity.class);
+        messenger.putExtra("switchView", "");
+
+        startActivity(messenger);
+    }
+
+    public void openCourse(View view) {
+
+        Intent messenger = new Intent(this, CourseActivity.class);
+        messenger.putExtra("switchView", "");
+
+        startActivity(messenger);
+    }
+
+    public void openAllocation(View view) {
+
+        Intent messenger = new Intent(this, AllocationActivity.class);
+        messenger.putExtra("switchView", "");
+
+        startActivity(messenger);
     }
 }
