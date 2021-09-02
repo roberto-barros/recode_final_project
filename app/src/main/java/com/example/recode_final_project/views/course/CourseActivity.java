ed1ceecc20964e1b455c5515b8a84dfeba52eee2
roberto-barros <jroberto.barros2506@gmail.com>
2021-09-02 19:09:35 -0300

    Melhorias de layout

--- app/src/main/java/com/example/recode_final_project/views/course/CourseActivity.java
+++ app/src/main/java/com/example/recode_final_project/views/course/CourseActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
